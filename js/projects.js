a9df1db05cd27ede9b29d1fd9ddd224f24fd77e6
Student <example@example.com>
2018-08-03 12:05:01 -0700

    Create function to fill project panels from JS object

---
+++ js/projects.js
@@ -0,0 +1,239 @@
+var projectData =
+{
+  'frogracer':{
+        bgColor:'#002508',
+        url:'http://www.eggborne.com/frog-racer',
+        iconUrl:'/icons/frogracericon.png',
+        shortName:'frogracer',
+        displayName:'Frog Racer 2018',
+        descriptionBullets:[
+            'Endless randomized Battletoads Turbo Tunnel level',
+            'Split-screen mobile touch control interface',
+        ],
+        techBullets:[
+            'HTML5',
+            'JavaScript',
+            'CSS',
+            'jQuery',
+            'PIXI\.js',
+            'Howler\.js',
+            'PHP',
+            'mySQL',
+        ],
+        screenshots:['img/frtitle.png']
+    },
+    'tetro':{
+        bgColor:'#150018',
+        url:'https://eggborne,github.io/tetro/',
+        iconUrl:'/icons/tetrisicon.bmp',
+        shortName:'tetro',
+        displayName:'Tetro',
+        descriptionBullets:[
+            'Mobile-optimized recreation of the original NES Tetris with new features and game modes',
+        ],
+        techBullets:[
+            'HTML5',
+            'JavaScript',
+            'CSS',
+            'jQuery',
+            'PIXI.js',
+            'Howler.js',
+            'PHP',
+            'mySQL',
+        ],
+        screenshots:['img/tetrotitle.png']
+    },
+    'metroid':{
+        bgColor:'#00004d',
+        url:'http://www.eggborne.com/metroid/',
+        shortName:'metroid',
+        iconUrl:'/icons/metroidicon.png',
+        displayName:'Random Metroid',
+        descriptionBullets:[
+            'Procedurally generated NES Metroid',
+            'Dynamic asset loading system ensures optimum framerate regardless of map size'
+        ],
+        techBullets:[
+            'HTML5',
+            'JavaScript',
+            'CSS',
+            'PIXI.js'
+        ],
+        screenshots:['img/metroidtitle.png']
+    },
+    'kungfu':{
+      bgColor:'#00004d',
+      url:'http://www.eggborne.com/kungfu/',
+      shortName:'kungfu',
+      iconUrl:'/icons/kficon.png',
+      displayName:'NES Kung Fu',
+      descriptionBullets:[
+        'HTML5 recreation of NES Kung Fu',
+      ],
+      techBullets:[
+          'HTML5',
+          'JavaScript',
+          'CSS',
+          'jQuery',
+          'PIXI.js',
+          'Howler.js',
+          'PHP',
+          'mySQL'
+      ],
+      screenshots:['']
+    },
+    'punchout':{
+      bgColor:'#804000',
+      url:'http://www.eggborne.com/mtpo/',
+      shortName:'punchout',
+      iconUrl:'/icons/boxinggloveicon.png',
+      displayName:'Donald Trumps\'s Punch-Out!',
+      descriptionBullets:[
+        'Mike Tyson\'s Punch-Out except you beat up Trump',
+        'Only makes you feel better temporarily'
+      ],
+      techBullets:[
+          'HTML5',
+          'JavaScript',
+          'CSS',
+          'PIXI.js',
+          'Howler.js'
+      ],
+      screenshots:['img/punchouttitle.png']
+    },
+    'toad':{
+      bgColor:'#002508',
+      url:'http://www.eggborne.com/toad/',
+      iconUrl:'/icons/toadicon.png',
+      shortName:'toad',
+      displayName:'Toad\'s Odyssey',
+      descriptionBullets:[
+        ''
+      ],
+      techBullets:[
+          'HTML5',
+          'JavaScript',
+          'CSS',
+          'PIXI.js',
+          'Howler.js'
+      ],
+      screenshots:['']
+    },
+    'cas':{
+      bgColor:'#222211',
+      url:'https://eggborne.github.io/cellular-automata-sandbox/',
+      iconUrl:'/icons/casicon.png',
+      shortName:'cas',
+      displayName:'Cellular Automata Sandbox',
+      descriptionBullets:[
+        'Creates cool 3D designs using procedural generation',
+        'Highly customizable in appearance and logic'
+      ],
+      techBullets:[
+          'HTML5',
+          'JavaScript',
+          'CSS',
+          'PIXI.js'
+      ],
+      screenshots:['img/castitle.png']
+    },
+    'eggprotector':{
+      bgColor:'#1a0000',
+      url:'http://www.eggborne.com/eggprotector/',
+      iconUrl:'/icons/fetusicon.png',
+      shortName:'eggprotector',
+      displayName:'Egg Protector',
+      descriptionBullets:[
+        'Missile Command/tower defense mashup with a biological twist'
+      ],
+      techBullets:[
+          'HTML5',
+          'JavaScript',
+          'CSS',
+          'PIXI.js',
+          'Howler.js'
+      ],
+      screenshots:['']
+    },
+    'spermtyper':{
+      bgColor:'#002b2b',
+      iconUrl:'/icons/spermtypericon.png',
+      url:'http://www.eggborne.com/spermtyper/',
+      shortName:'spermtyper',
+      displayName:'Sperm Typer',
+      descriptionBullets:[
+        'Fast-paced typing game',
+        'Retrieves an endless list of unique words from the Reddit API',
+        'Requires landscape mode and physical keyboard'
+      ],
+      techBullets:[
+          'HTML5',
+          'JavaScript',
+          'jQuery',
+          'CSS',
+          'PIXI.js',
+          'Howler.js'
+      ],
+      screenshots:['']
+    },
+    'yarsrevenge':{
+      bgColor:'#252525',
+      url:'http://www.eggborne.com/yars/',
+      iconUrl:'/icons/yarsicon.png',
+      shortName:'yarsrevenge',
+      displayName:'Yars\' Revenge',
+      descriptionBullets:[
+        ''
+      ],
+      techBullets:[
+          'HTML5',
+          'JavaScript',
+          'CSS',
+          'PIXI.js',
+          'Howler.js'
+      ],
+      screenshots:['']
+    },
+    'clues':{
+      bgColor:'#252525',
+      url:'http://www.eggborne.com/clues/',
+      iconUrl:'/icons/crosswordicon.png',
+      shortName:'clues',
+      displayName:'Crossword Clue Creator',
+      descriptionBullets:[
+        ''
+      ],
+      techBullets:[
+          'HTML5',
+          'JavaScript',
+          'jQuery',
+          'CSS',
+          'PIXI.js',
+          'Howler.js',
+          'PHP',
+          'mySQL',
+      ],
+      screenshots:['']
+    }
+    ,
+    'pong':{
+      bgColor:'#252525',
+      url:'http://www.eggborne.com/pong/',
+      iconUrl:'/icons/pongicon.png',
+      shortName:'pong',
+      displayName:'SQL Pong',
+      descriptionBullets:[
+        'Online two-player Pong',
+        'Powered by a mySQL database'
+      ],
+      techBullets:[
+          'HTML5',
+          'JavaScript',
+          'jQuery',
+          'CSS',
+          'PHP',
+          'mySQL',
+      ],
+      screenshots:['']
+    }
+}
--- js/script.js
+++ js/script.js
@@ -1,170 +1,34 @@
-var projectData =
-{
-  'frogracer':{
-        bgColor:'#002508',
-        url:'/frogracer/',
-        iconUrl:'/icons/frogracericon.png',
-        shortName:'frogracer',
-        displayName:'Frog Racer 2018',
-        descriptionBullets:[
-            'Endless randomized Battletoads Turbo Tunnel level',
-            'Split-screen mobile touch control interface',
-        ],
-        screenshots:['/img/frtitle.png']
-    },
-    'tetro':{
-        bgColor:'#150018',
-        url:'/tetro/',
-        iconUrl:'/icons/tetrisicon.bmp',
-        shortName:'tetro',
-        displayName:'Tetro',
-        descriptionBullets:[
-            'Mobile-optimized recreation of the original NES Tetris with new features and game modes',
-        ],
-        screenshots:['/img/tetrotitle.png']
-    },
-    'metroid':{
-        bgColor:'#00004d',
-        url:'/metroid/',
-        shortName:'metroid',
-        iconUrl:'/icons/metroidicon.png',
-        displayName:'Random Metroid',
-        descriptionBullets:[
-            'Procedurally generated NES Metroid',
-            'Dynamic multi-level loading system ensures optimum framerate regardless of map size'
-        ],
-        screenshots:['/img/metroidtitle.png']
-    },
-    'kungfu':{
-        bgColor:'#00004d',
-        url:'/kungfu/',
-        shortName:'kungfu',
-        iconUrl:'/icons/kficon.png',
-        displayName:'NES Kung Fu',
-        descriptionBullets:[
-            'Procedurally generated NES Metroid',
-            'Dynamic multi-level loading system ensures optimum framerate regardless of map size'
-        ],
-        screenshots:['/img/metroidtitle.png']
-    },
-    'punchout':{
-        bgColor:'#804000',
-        url:'/mtpo/',
-        shortName:'punchout',
-        iconUrl:'/icons/boxinggloveicon.png',
-        displayName:'Donald Trumps\'s Punch-Out!',
-        descriptionBullets:[
-            'Mike Tyson\'s Punch-Out except you beat up Trump',
-            // '',
-            'Only makes you feel better temporarily'
-        ],
-        screenshots:['/img/punchouttitle.png']
-    },
-    'toad':{
-        bgColor:'#002508',
-        url:'/toad/',
-        iconUrl:'/icons/toadicon.png',
-        shortName:'toad',
-        displayName:'Toad\'s Odyssey',
-        descriptionBullets:[
-            '',
-        ],
-        screenshots:['']
-    },
-    'cas':{
-        bgColor:'#222211',
-        url:'/cas/',
-        iconUrl:'/icons/casicon.png',
-        shortName:'cas',
-        displayName:'Cellular Automata Sandbox',
-        descriptionBullets:[
-            "Creates cool 3D designs using procedural generation",
-            "Highly customizable in appearance and logic"
-        ],
-        screenshots:['/img/castitle.png']
-    },
-    'eggprotector':{
-        bgColor:'#1a0000',
-        url:'/eggprotector/',
-        iconUrl:'/icons/fetusicon.png',
-        shortName:'eggprotector',
-        displayName:'Egg Protector',
-        descriptionBullets:[
-            'Missile Command/tower defense mashup with a biological twist'
-        ],
-        screenshots:['/img/kctitle.png']
-    },
-    'spermtyper':{
-        bgColor:'#002b2b',
-        iconUrl:'/icons/spermtypericon.png',
-        url:'/spermtyper/',
-        shortName:'spermtyper',
-        displayName:'Sperm Typer',
-        descriptionBullets:[
-            'Fast-paced typing game',
-            'Requires landscape mode and physical keyboard'
-        ],
-        screenshots:['/img/sttitle.png']
-    },
-    'yarsrevenge':{
-        bgColor:'#252525',
-        url:'/yars/',
-        iconUrl:'/icons/yarsicon.png',
-        shortName:'yarsrevenge',
-        displayName:'Yars\' Revenge',
-        descriptionBullets:[
-            'Yars\' Revenge'
-        ],
-        screenshots:['/img/yarstitle.png']
-    },
-    'clues':{
-        bgColor:'#252525',
-        url:'/clues/',
-        iconUrl:'/icons/crosswordicon.png',
-        shortName:'clues',
-        displayName:'Crossword Clue Creator',
-        descriptionBullets:[
-
-        ],
-        screenshots:[]
-    }
-    ,
-    'pong':{
-        bgColor:'#252525',
-        url:'/pong/',
-        iconUrl:'/icons/pongicon.png',
-        shortName:'pong',
-        displayName:'SQL Pong',
-        descriptionBullets:[
-
-        ],
-        screenshots:[]
-    },
-    'cursorsforcats':{
-        bgColor:'#252525',
-        url:'http://www.cursorsforcats.com/',
-        iconUrl:'/icons/cfcicon.png',
-        shortName:'cursorsforcats',
-        displayName:'Cursors for Cats',
-        descriptionBullets:[
-
-        ],
-        screenshots:[]
-    }
-}
-
-
 function fillProjectCards() {
   var projectList = Object.keys(projectData)
   console.log(projectList)
   for (var p=0;p<projectList.length;p++) {
     var currentProjectData = projectData[projectList[p]]
+    var screenshotPath = currentProjectData.screenshots[0].length > 2 ? currentProjectData.screenshots[0] : "img/placeholder.png"
+    var displayName = currentProjectData.displayName
     console.log("current project is " + currentProjectData.shortName)
-    var targetDivID = (p%2===0) ? "column1" : "column2"
+    var targetColumnID = (p%2===0) ? "column1" : "column2"
     var projectID = "project"+p
-    console.log("putting in column " + targetDivID + " with ID " + projectID)
-    var targetDiv = document.getElementById(targetDivID)
-    targetDiv.innerHTML += '<div id="'+projectID+'" class="post panel panel-success"><div class="panel-heading"><h2 class="panel-title"><a href="#">Project Title</a></h2></div><div class="panel-body"><img class="screenshot" src="img/placeholder.bmp" alt="Project screenshot"><div style="margin: 20px 0 15px 0" class="page-header"><h4>Description:</h4></div><p class="project-description">Description body. Monkeys like bananas. Sometimes dogs are brown. I would like a chicken as a pet.</p><br><div style="margin: 20px 0 15px 0" class="page-header"><h4>Technologies used:</h4></div><ul class="tech-list"></ul></div></div>'
+    console.log("putting in column " + targetColumnID + " with ID " + projectID)
+    var targetDiv = document.getElementById(targetColumnID)
+    targetDiv.innerHTML += '<div id="'+projectID+'" class="post panel panel-success"><div class="panel-heading" style="background:'+currentProjectData.bgColor+'"><h2 class="panel-title"><a href="'+currentProjectData.url+'"><h3>'+displayName+'</h3></a></h2></div><div class="panel-body"><img class="screenshot" src="'+screenshotPath+'" alt="'+displayName+' screenshot"><div style="margin: 20px 0 15px 0" class="page-header"><h4>Description:</h4></div><p class="project-description"><ul class="description-bullets"></ul></p><br><div style="margin: 20px 0 15px 0" class="page-header"><h4>Technologies used:</h4></div><ul class="tech-bullets"></ul></div><div class="panel-footer"><div class="row"><div class="col-md-4"><span style="float:left"><a href="#">Website link</a></span></div><div class="col-md-5"></div><div class="col-md-3"><span style="float:right"><a href="#">Github link</a></span></div></div></div></div>'
     console.log(targetDiv)
+    var descriptionList = document.getElementsByClassName("description-bullets")[document.getElementsByClassName("description-bullets").length-1]
+    for (var d=0;d<currentProjectData.descriptionBullets.length;d++) {
+      var currentBullet = currentProjectData.descriptionBullets[d]
+      if (currentBullet.length) {
+        descriptionList.innerHTML += '<li>'+currentBullet+'</li>'
+      }
+    }
+    var techList = document.getElementsByClassName("tech-bullets")[document.getElementsByClassName("tech-bullets").length-1]
+    console.log("found " + document.getElementsByClassName("tech-bullets").length + " tech lists")
+    console.log(currentProjectData.techBullets.length + " bullets for " + displayName)
+    console.log("adding to")
+    console.log(techList)
+    for (var t=0;t<currentProjectData.techBullets.length;t++) {
+      var currentTechBullet = currentProjectData.techBullets[t]
+      console.log("about to add "+currentTechBullet+" to tech list for " + displayName)
+      // doesn't add them correctly!!
+      techList.innerHTML += '<li>'+currentTechBullet+'</li>'
+    }
   }
 }
