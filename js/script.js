f9e2d5bdde5a2f30d65ccdf416914bc62eef5d84
Michael Donovan <mike@eggborne.com>
2018-08-03 23:50:30 -0700

    Create a JS function to provide the three sections' HTML to DOM; add transition effect to gallery

--- js/script.js
+++ js/script.js
@@ -1,3 +1,9 @@
+var currentPage = 0
+var sectionBodies = {
+  projects : '<div class="row"><div id="column1" class="col-sm-6"></div><div id="column2" class="col-sm-6"></div></div>',
+  about : '<div class="page-header"><h1>About Mike</h1></div> <p> <div class="page-header"><h1>Education</h1></div> Nullam bibendum mi dapibus, pretium mauris non, porttitor nisl. Quisque mollis tempus semper. Etiam convallis vehicula neque, sit amet mollis nibh posuere vel. Aenean viverra arcu id congue dignissim. Mauris diam lorem, condimentum vel diam quis, dapibus sollicitudin ligula. Fusce sed luctus ante. Sed sit amet eros tempor, sollicitudin erat id, luctus leo. Curabitur eget aliquet dui, et sollicitudin turpis. Aenean eget molestie risus, eu pulvinar lorem. Aliquam sed velit dolor. Curabitur vel elit felis. Nam viverra dui nisi, quis dictum massa euismod ac. Proin sit amet tempus elit. Donec sit amet arcu ornare, vehicula quam id, condimentum nisi. Donec fringilla quis lorem in condimentum. </p> <p> <div class="page-header"><h1>Skills</h1></div> In vulputate pretium risus, in pharetra libero tempor et. Fusce posuere orci quis dolor sodales interdum. Sed ultricies sodales purus, at consequat tellus tristique non. Integer id arcu ut nisi egestas interdum a sed neque. Nam ut leo ut odio porta finibus id nec dui. Suspendisse potenti. Mauris eros urna, ullamcorper a luctus interdum, varius quis tortor. Praesent imperdiet, enim at egestas vulputate, lectus sem varius quam, vitae dapibus mauris leo non lacus. </p> <p> <div class="page-header"><h1>Hobbies</h1></div> Morbi at commodo risus. Quisque ornare vel velit sed euismod. Fusce pharetra commodo urna, tincidunt mattis odio elementum vel. Proin eget facilisis magna. Vestibulum et dui quis sem auctor malesuada. Sed nec est viverra, placerat arcu et, lobortis lectus. Quisque ac metus semper, dapibus lorem nec, fermentum augue. Nullam fringilla pellentesque lacus in tempor. Suspendisse gravida fringilla nulla ac venenatis. Nulla elementum feugiat sollicitudin. </p>',
+  contact : '<div class="page-header"><h1>Contact Mike</h1></div> <div class="row"> <div class="col-lg-4 contact-grid"> <h2>Phone:</h2> <h3><strong>360-936-8442</strong></h3> </div> <div class="col-lg-4 contact-grid"> <h2>Email:</h2> <h3><a href="mailto:mike@eggborne.com"><strong>mike@eggborne.com</strong></a></h3> </div> <div class="col-lg-4 contact-grid"> <h2>GitHub:</h2> <h3><a href="https://www.github.com/eggborne"><strong>github.com/eggborne</strong></a></h3> </div> </div>'
+}
 function fillProjectCards() {
   var projectList = Object.keys(projectData)
   for (var p=0;p<projectList.length;p++) {
@@ -36,7 +42,6 @@ function toggleBarButton(button) {
       currentButton.classList.add('btn-default')
     }
   }
-  loadSection(button.id)
 }
 function expandOnClick(sectionID,index) {
   var section = document.getElementById(sectionID+index)
@@ -53,15 +58,32 @@ function expandOnClick(sectionID,index) {
     button.innerHTML = "Expand"
   }
 }
-function loadSection(buttonID) {
-  if (buttonID==="projects-button") {
-    console.log("projects clicked")
-    window.location.replace("index.html")
-  } else if (buttonID==="about-button") {
-    console.log("about clicked")
-    window.location.replace("aboutme.html")
+function loadSection(section) {
+  if (section==="projects") {
+    // this only occurs on initial page load
+    document.getElementById("gallery").innerHTML = sectionBodies[section]
+    document.getElementById("gallery").style.transform = "translateX(0)"
+    toggleGallery("on")
   } else {
-    console.log("contact clicked")
-    window.location.replace("contact.html")
+    document.getElementById("page-body").innerHTML = sectionBodies[section]
   }
+  currentPage = Object.keys(sectionBodies).indexOf(section)
+}
+function toggleGallery(setting) {
+  var galleryDiv = document.getElementById("gallery")
+  console.log("toggling " + setting)
+  if (setting==="on" || galleryDiv.style.opacity===0) {
+    console.log("showing gallery")
+    galleryDiv.style.position = "initial"
+    galleryDiv.style.opacity = 1
+    galleryDiv.style.transform = "translateX(0)"
+  } else {
+    galleryDiv.style.opacity = 0
+    galleryDiv.style.transform = "translateY(100vh)"
+    setTimeout(function(){
+      galleryDiv.style.position = "fixed"
+    },200)
+    
+  }
+  console.log(galleryDiv)
 }
