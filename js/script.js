5009ba5d48fef8f18ab4a4cfd06bf00d6b038d65
Michael Donovan <mike@eggborne.com>
2018-08-04 17:38:02 -0700

    Create function to determine initial column amount according to screen width

--- js/script.js
+++ js/script.js
@@ -1,16 +1,19 @@
 var currentPage = 0
+var maxColumns = 4
 var sectionBodies = {
-  projects : '<div class="row"><div id="column1" class="col-sm-6"></div><div id="column2" class="col-sm-6"></div></div>',
+  projects : '<div class="row"><div id="column1" class="col-sm-'+(12/maxColumns)+'"></div><div id="column1" class="col-sm-'+(12/maxColumns)+'"></div><div id="column1" class="col-sm-'+(12/maxColumns)+'"></div><div id="column2" class="col-sm-'+(12/maxColumns)+'"></div></div>',
   about : '<div class="page-header"><h1>About Mike</h1></div> <p> <div class="page-header"><h1>Education</h1></div> Nullam bibendum mi dapibus, pretium mauris non, porttitor nisl. Quisque mollis tempus semper. Etiam convallis vehicula neque, sit amet mollis nibh posuere vel. Aenean viverra arcu id congue dignissim. Mauris diam lorem, condimentum vel diam quis, dapibus sollicitudin ligula. Fusce sed luctus ante. Sed sit amet eros tempor, sollicitudin erat id, luctus leo. Curabitur eget aliquet dui, et sollicitudin turpis. Aenean eget molestie risus, eu pulvinar lorem. Aliquam sed velit dolor. Curabitur vel elit felis. Nam viverra dui nisi, quis dictum massa euismod ac. Proin sit amet tempus elit. Donec sit amet arcu ornare, vehicula quam id, condimentum nisi. Donec fringilla quis lorem in condimentum. </p> <p> <div class="page-header"><h1>Skills</h1></div> In vulputate pretium risus, in pharetra libero tempor et. Fusce posuere orci quis dolor sodales interdum. Sed ultricies sodales purus, at consequat tellus tristique non. Integer id arcu ut nisi egestas interdum a sed neque. Nam ut leo ut odio porta finibus id nec dui. Suspendisse potenti. Mauris eros urna, ullamcorper a luctus interdum, varius quis tortor. Praesent imperdiet, enim at egestas vulputate, lectus sem varius quam, vitae dapibus mauris leo non lacus. </p> <p> <div class="page-header"><h1>Hobbies</h1></div> Morbi at commodo risus. Quisque ornare vel velit sed euismod. Fusce pharetra commodo urna, tincidunt mattis odio elementum vel. Proin eget facilisis magna. Vestibulum et dui quis sem auctor malesuada. Sed nec est viverra, placerat arcu et, lobortis lectus. Quisque ac metus semper, dapibus lorem nec, fermentum augue. Nullam fringilla pellentesque lacus in tempor. Suspendisse gravida fringilla nulla ac venenatis. Nulla elementum feugiat sollicitudin. </p>',
   contact : '<div class="page-header"><h1>Contact Mike</h1></div> <div class="row"> <div class="col-lg-4 contact-grid"> <h2>Phone:</h2> <h3><strong>360-936-8442</strong></h3> </div> <div class="col-lg-4 contact-grid"> <h2>Email:</h2> <h3><a href="mailto:mike@eggborne.com"><strong>mike@eggborne.com</strong></a></h3> </div> <div class="col-lg-4 contact-grid"> <h2>GitHub:</h2> <h3><a href="https://www.github.com/eggborne"><strong>github.com/eggborne</strong></a></h3> </div> </div>'
 }
 function fillProjectCards() {
   var projectList = Object.keys(projectData)
+  var cardsPerColumn = Math.floor(projectList.length/4) // move to next every nth project
+  var targetColumnID = 0
   for (var p=0;p<projectList.length;p++) {
     var currentProjectData = projectData[projectList[p]]
     var screenshotPath = currentProjectData.screenshots[0].length > 2 ? currentProjectData.screenshots[0] : "img/placeholder.png"
     var displayName = currentProjectData.displayName
-    var targetColumnID = (p%2===0) ? "column1" : "column2"
+    
     var projectID = "project"+p
     var repoURL = "https://www.github.com/eggborne/"+currentProjectData.repo
     var targetDiv = document.getElementById(targetColumnID)
