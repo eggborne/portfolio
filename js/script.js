8454309f7d2d583fd1ba635275fb2f02904776e2
Michael Donovan <mike@eggborne.com>
2018-08-05 00:49:03 -0700

    Refine column responsiveness

--- js/script.js
+++ js/script.js
@@ -1,36 +1,29 @@
 var currentSection = "projects" // must change to sections.projects once sections obj is made
 var minimumColumnWidth = 350
-var minimumMargin = 0.03 // how small the margin will go in case of narrow screen
 var maxColumns = 5
-var stageWidth = Math.floor(window.innerWidth*0.8) // 0.8 is from .container's 80% width
 var sectionBodies = { // include this in sections obj as sections.html
-  projects : '<div class="row">',
+  projects : '<div class="row">', // the rest is filled with determineColumnAmout()
   about : '<div class="page-header"><h1>About Mike</h1></div> <p> <div class="page-header"><h1>Education</h1></div> Nullam bibendum mi dapibus, pretium mauris non, porttitor nisl. Quisque mollis tempus semper. Etiam convallis vehicula neque, sit amet mollis nibh posuere vel. Aenean viverra arcu id congue dignissim. Mauris diam lorem, condimentum vel diam quis, dapibus sollicitudin ligula. Fusce sed luctus ante. Sed sit amet eros tempor, sollicitudin erat id, luctus leo. Curabitur eget aliquet dui, et sollicitudin turpis. Aenean eget molestie risus, eu pulvinar lorem. Aliquam sed velit dolor. Curabitur vel elit felis. Nam viverra dui nisi, quis dictum massa euismod ac. Proin sit amet tempus elit. Donec sit amet arcu ornare, vehicula quam id, condimentum nisi. Donec fringilla quis lorem in condimentum. </p> <p> <div class="page-header"><h1>Skills</h1></div> In vulputate pretium risus, in pharetra libero tempor et. Fusce posuere orci quis dolor sodales interdum. Sed ultricies sodales purus, at consequat tellus tristique non. Integer id arcu ut nisi egestas interdum a sed neque. Nam ut leo ut odio porta finibus id nec dui. Suspendisse potenti. Mauris eros urna, ullamcorper a luctus interdum, varius quis tortor. Praesent imperdiet, enim at egestas vulputate, lectus sem varius quam, vitae dapibus mauris leo non lacus. </p> <p> <div class="page-header"><h1>Hobbies</h1></div> Morbi at commodo risus. Quisque ornare vel velit sed euismod. Fusce pharetra commodo urna, tincidunt mattis odio elementum vel. Proin eget facilisis magna. Vestibulum et dui quis sem auctor malesuada. Sed nec est viverra, placerat arcu et, lobortis lectus. Quisque ac metus semper, dapibus lorem nec, fermentum augue. Nullam fringilla pellentesque lacus in tempor. Suspendisse gravida fringilla nulla ac venenatis. Nulla elementum feugiat sollicitudin. </p>',
-  contact : '<div class="page-header"><h1>Contact Mike</h1></div> <div class="row"> <div class="col-lg-4 contact-grid"> <h2>Phone:</h2> <h3><strong>360-936-8442</strong></h3> </div> <div class="col-lg-4 contact-grid"> <h2>Email:</h2> <h3><a href="mailto:mike@eggborne.com"><strong>mike@eggborne.com</strong></a></h3> </div> <div class="col-lg-4 contact-grid"> <h2>GitHub:</h2> <h3><a href="https://www.github.com/eggborne"><strong>github.com/eggborne</strong></a></h3> </div> </div>'
+  contact : '<div class="page-header"><h1>Contact Mike</h1></div> <div class="row"> <div class="col-xs-4 contact-grid"> <h2>Phone:</h2> <h3><strong>360-936-8442</strong></h3> </div> <div class="col-xs-4 contact-grid"> <h2>Email:</h2> <h3><a href="mailto:mike@eggborne.com"><strong>mike@eggborne.com</strong></a></h3> </div> <div class="col-xs-4 contact-grid"> <h2>GitHub:</h2> <h3><a href="https://www.github.com/eggborne"><strong>github.com/eggborne</strong></a></h3> </div> </div>'
 }
 
 function determineColumnAmount() {
+  var stageWidth = document.getElementById("stage").getBoundingClientRect().width
   log("minimumColumnWidth: " + minimumColumnWidth)
-  log("maxColumns: " + maxColumns,true)
-  if ((window.innerWidth*0.8) < minimumColumnWidth) {
-    log("width is " + stageWidth + ", smaller than min col width!")
-    if ((window.innerWidth*minimumMargin)-stageWidth >= minimumColumnWidth) {
-      stageWidth = Math.floor(window.innerWidth*newMargin)
-    } else {
-      log("changing col width to " + stageWidth)
-      minimumColumnWidth = stageWidth
-    }
-  } else {
-    log("stage width is " + stageWidth + ", full " + window.innerWidth)
+  log("maxColumns: " + maxColumns)
+  log("stage width: " + stageWidth)
+  log("full width: " + window.innerWidth,true)
+  if (minimumColumnWidth>stageWidth) {
+    minimumColumnWidth = stageWidth
+    log("had to reduce column width to " + minimumColumnWidth)
   }
   maxColumns = Math.floor(stageWidth/minimumColumnWidth)
   // make sure it's divisible by 12
   maxColumns -= (12 % maxColumns)
   log("room for " + maxColumns + " columns")
-  log("each should be col-" + (12/maxColumns))
-  // add maxColumns * column divs
+  log("each should be col-" + (12/maxColumns),true)
   for (var c=0;c<maxColumns;c++) {
-    sectionBodies.projects += '<div id="column'+c+'" class="col-sm-'+(12/maxColumns)+'"></div>'
+    sectionBodies.projects += '<div id="column'+c+'" class="col-xs-'+(12/maxColumns)+'"></div>'
   }
   sectionBodies.projects += '</div>'
 }
